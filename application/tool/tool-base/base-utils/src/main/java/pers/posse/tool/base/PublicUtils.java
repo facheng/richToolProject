fcfc022c40e041e026ebee963c7b77a2f04a3315
Posse Wang <posse.wang@otms.com>
2017-07-17 19:17:07 +0800

    richTool init.

---
+++ application/tool/tool-base/base-utils/src/main/java/pers/posse/tool/base/PublicUtils.java
@@ -0,0 +1,10 @@
+package pers.posse.tool.base;
+
+/**
+ * Created by posse on 17-7-17.
+ */
+public class PublicUtils {
+    public static String getPuclicUtilsStr() {
+        return "this is tool base - base utils - PublicUtils.";
+    }
+}
